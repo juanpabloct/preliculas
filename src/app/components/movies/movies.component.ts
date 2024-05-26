c92e80e5224cfb444d3116cae823ffd1c1ee68c6
juanpabloct <juanptorres2003@gmail.com>
2024-05-26 14:06:19 -0500

    feat(main): resolve bugs and edit styles

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -21,50 +21,47 @@ import { GetMovies } from '../../../models/peliculas';
 export class MoviesComponent {
   search = signal('');
   data = signal<GetMovies['results']>([]);
-  private copyData = signal<GetMovies['results']>([]);
   page = signal(1);
-  totalPage = signal(0);
+  totalPage = signal(1);
 
   async getMoviesService() {
     const { results } = await getMovies(this.page());
-    this.copyData.set(results);
     this.data.set(results);
+    this.totalPage.set(5000)
   }
   async ngOnInit() {
-    this.getMoviesService();
+    await this.getMoviesService();
   }
 
   async setPagination(newPage: number) {
     this.page.set(newPage);
     this.data.set((await getMovies(newPage)).results);
   }
-  clearSearch() {
+  async clearSearch() {
     this.search.set('');
+    await this.getMoviesService()
   }
-  onSearch() {
+  async onSearch() {
     const search = this.search();
-    if (search.length > 0) {
-      (async () => {
-        const { results, total_pages } = await searchMovies(
-          search,
-          this.page()
-        );
-        this.data.set(results);
-        this.page.set(1);
-        this.totalPage.set(total_pages);
-      })();
-    } else if (this.data().length !== 0 && search.length === 0) {
-      this.data.set(this.copyData());
-    }
+    await (async () => {
+      const { results, total_pages } = await searchMovies(
+        search,
+        this.page()
+      );
+      this.data.set(results);
+      this.page.set(1);
+      this.totalPage.set(total_pages);
+    })();
+
   }
   trackByFn(index: number, item: any): any {
-    return item.id; // Suponiendo que item tiene una propiedad 'id' que es única
+    return item.id;
   }
 
-  onKeyPress(event: KeyboardEvent) {
+  async onKeyPress(event: KeyboardEvent) {
     const key = event.key;
     if (key === 'Enter') {
-      this.onSearch();
+      await this.onSearch();
     }
   }
 }
--- src/app/components/target/target.component.ts
+++ src/app/components/target/target.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ResultMovies } from '../../../models/peliculas';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-target',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterLink],
   templateUrl: './target.component.html',
   styleUrl: './target.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
